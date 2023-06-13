1dc000b53e9b87aa635b33187b25bf5c2ac476b5
Giulio Pedicone <pediconegiulio02@gmail.com>
2023-06-13 09:11:33 +0200

    Add Shell exercises

---
+++ Tutorato/2023-06-13/function.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+function hello() {
+    [ $# -lt 1 ] && return 1
+    [ -z "$1" ] && return 2
+    local name=$1
+    
+    echo "Hello $name"
+    return 0
+}
+
+hello function
+echo "exit status: $?"
+
+result=$(hello function)
+echo $result
---
+++ Tutorato/2023-06-13/while.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+i=0
+while [ $i -lt 10 ]
+do
+echo -n "$i "
+((i++)) # oppure i=$((i+1))
+done
+echo ""
+
+# Si esegue con bash if.sh
