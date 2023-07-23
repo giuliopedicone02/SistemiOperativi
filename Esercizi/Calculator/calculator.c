b537c7747c23a1aad4bf95f4a6735944cdcf8770
Giulio Pedicone <pediconegiulio02@gmail.com>
2023-07-23 13:00:07 +0200

    Add Calculator

--- Esercizi/Calculator/calculator.c
+++ Esercizi/Calculator/calculator.c
@@ -248,7 +248,7 @@ int main(int argc, char **argv)
 
     if (argc != 2)
     {
-        printf("Usare %s, <file.txt>\n", argv[0]);
+        printf("Usage: %s <file.txt>\n", argv[0]);
         exit(EXIT_FAILURE);
     }
 
