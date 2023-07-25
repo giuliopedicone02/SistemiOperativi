5a1db620e6a42a2be72c85ca36cfb1cbcbd6faea
Giulio Pedicone <pediconegiulio02@gmail.com>
2023-07-25 10:37:25 +0200

    Add soluzione my-fgrep

--- Tutorato/my-fgrep/my-fgrep.c
+++ Tutorato/my-fgrep/my-fgrep.c
@@ -1,435 +1,392 @@
 #define _GNU_SOURCE
-#include "../lib-misc.h"
-#include <stdlib.h>
-#include <semaphore.h>
+#include "../../lib/lib-misc.h"
+#include <fcntl.h>
 #include <pthread.h>
 #include <stdbool.h>
-
+#include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
-#include <ctype.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#define BUFFER_SIZE 1024
 
 typedef struct
 {
-    int fileattuale;
-    bool v;
-    bool i;
+    char buffer[BUFFER_SIZE];
     bool done;
-    char parola[512];
-    int turn;
-    char riga[1024];
+    unsigned turn;
+
+    unsigned nreader;
     pthread_mutex_t lock;
-    pthread_cond_t *cond;
-} sharedRF;
+    pthread_cond_t *pcond;
+} shared_rf;
+
+void init_shared_rf(shared_rf *s, unsigned nreader)
+{
+    int err;
+
+    s->done = 0;
+    s->turn = 1;
+    s->nreader = nreader;
+
+    if ((err = pthread_mutex_init(&s->lock, NULL)) != 0)
+        exit_with_err("pthread_mutex_init", err);
+
+    s->pcond = malloc(sizeof(pthread_cond_t) * (nreader + 1));
+
+    for (unsigned i = 0; i < nreader + 1; i++)
+        if ((err = pthread_cond_init(&s->pcond[i], NULL)) != 0)
+            exit_with_err("pthread_cond_init", err);
+}
+
+void destroy_shared_rf(shared_rf *s)
+{
+    pthread_mutex_destroy(&s->lock);
+
+    for (unsigned i = 0; i < s->nreader + 1; i++)
+        pthread_cond_destroy(&s->pcond[i]);
+
+    free(s->pcond);
+    free(s);
+}
 
 typedef struct
 {
-    char riga[1024];
+    char buffer[BUFFER_SIZE];
+    bool turn;
     bool done;
-    pthread_mutex_t lock2;
-    pthread_cond_t cond_filter;
-    pthread_cond_t cond_main;
-} sharedFM;
+
+    pthread_mutex_t lock;
+    pthread_cond_t pcond[2];
+    pthread_barrier_t barrier;
+} shared_fw;
+
+void init_shared_fw(shared_fw *s)
+{
+    int err;
+
+    s->turn = s->done = 0;
+
+    if ((err = pthread_mutex_init(&s->lock, NULL)) != 0)
+        exit_with_err("pthread_mutex_init", err);
+
+    for (int i = 0; i < 2; i++)
+        if ((err = pthread_cond_init(&s->pcond[i], NULL)) != 0)
+            exit_with_err("pthread_cond_init", err);
+
+    if ((err = pthread_barrier_init(&s->barrier, NULL, 3)) != 0)
+        exit_with_err("pthread_barrier_init", err);
+}
+
+void destroy_shared_fw(shared_fw *s)
+{
+    pthread_mutex_destroy(&s->lock);
+
+    for (int i = 0; i < 2; i++)
+        pthread_cond_destroy(&s->pcond[i]);
+
+    pthread_barrier_destroy(&s->barrier);
+
+    free(s);
+}
 
 typedef struct
 {
+    // dati privati
     pthread_t tid;
-    char *file;
-    int thread_name;
-    sharedRF *RF;
-    sharedFM *FM;
-    int numerofile;
+    unsigned thread_n;
+    char *filename;
+    char *word;
+    bool i_flag;
+    bool v_flag;
+
+    // dati condivisi
+    shared_rf *srf;
+    shared_fw *sfw;
 } thread_data;
 
-bool applicaFiltro(char *parola, char *riga, bool i, bool v)
-{
-    if (i == true && v == true)
-    {
-        if ((strcasestr(riga, parola) != NULL) || (strstr(riga, parola) == NULL))
-        {
-            return true;
-        }
-        else
-        {
-            return false;
-        }
-    }
-
-    if (i == false && v == true)
-    {
-        if (strstr(riga, parola) == NULL)
-        {
-            return true;
-        }
-        else
-        {
-            return false;
-        }
-    }
-
-    if (i == true && v == false)
-    {
-        if (strcasestr(riga, parola) != NULL)
-        {
-            return true;
-        }
-        else
-        {
-            return false;
-        }
-    }
-
-    if (i == false && v == false)
-    {
-        if (strstr(riga, parola) != NULL)
-        {
-            return true;
-        }
-        else
-        {
-            return false;
-        }
-    }
-}
-
-void Lettore(void *arg)
+bool reader_put_line(thread_data *td, char *strt)
 {
     int err;
-    char buffer[1024];
-    thread_data *sh = (thread_data *)arg;
+    char *line;
+    bool ret_value = 1;
 
-    FILE *fp = fopen(sh->file, "r");
-    if (!fp)
+    if ((err = pthread_mutex_lock(&td->srf->lock)) != 0)
+        exit_with_err("pthread_mutex_lock", err);
+
+    while (td->srf->turn != td->thread_n)
+        if ((err = pthread_cond_wait(&td->srf->pcond[td->thread_n],
+                                     &td->srf->lock)) != 0)
+            exit_with_err("pthread_cond_wait", err);
+
+    if ((line = strtok(strt, "\n")) == NULL)
     {
-        printf("Errore nell'apertura del file: %s", sh->file);
-        exit(EXIT_FAILURE);
+        ret_value = 0;
+        td->srf->done = 1;
     }
+    else
+        strncpy(td->srf->buffer, line, BUFFER_SIZE);
+
+    td->srf->turn = 0;
+
+    if ((err = pthread_cond_signal(&td->srf->pcond[0])) != 0)
+        exit_with_err("pthread_cond_signal", err);
+
+    if ((err = pthread_mutex_unlock(&td->srf->lock)) != 0)
+        exit_with_err("pthread_mutex_unlock", err);
+
+    return ret_value;
+}
+
+void reader(void *arg)
+{
+    thread_data *td = (thread_data *)arg;
+    int err, fd;
+    char *map, *strt;
+    struct stat statbuf;
+
+    if ((fd = open(td->filename, O_RDONLY)) == -1)
+        exit_with_sys_err("open");
+
+    if (fstat(fd, &statbuf) == -1)
+        exit_with_sys_err("fstat");
+
+    if ((map = mmap(NULL, statbuf.st_size, PROT_READ | PROT_WRITE, MAP_PRIVATE,
+                    fd, 0)) == NULL)
+        exit_with_sys_err("mmap");
+
+    if (close(fd) == -1)
+        exit_with_sys_err("close");
+
+    reader_put_line(td, map);
+
+    while (reader_put_line(td, NULL))
+        ;
+
+    munmap(map, statbuf.st_size);
+    pthread_exit(NULL);
+}
+
+bool filter_pass(thread_data *td, char *line)
+{
+    char *word = NULL;
+
+    if (td->i_flag)
+        word = strcasestr(line, td->word);
+    else
+        word = strstr(line, td->word);
+
+    if (td->v_flag)
+        return word == NULL;
+    else
+        return word != NULL;
+}
+
+void filterer(void *arg)
+{
+    int err;
+    thread_data *td = (thread_data *)arg;
+    unsigned actual_reader = 1;
+    char *line = NULL;
+    char buffer[BUFFER_SIZE];
+
     while (1)
     {
-        while (fgets(buffer, 1024, fp))
-        {
-            if ((err = pthread_mutex_lock(&sh->RF->lock)) != 0)
-            {
-                exit_with_err("pthread_mutex_lock", err);
-            }
-            while (sh->RF->turn != sh->thread_name)
-            {
-                if ((err = pthread_cond_wait(&sh->RF->cond[sh->thread_name], &sh->RF->lock)) != 0)
-                {
-                    exit_with_err("pthread_cond_wait", err);
-                }
-            }
-            if (buffer[strlen(buffer) - 1] == '\n')
-                buffer[strlen(buffer) - 1] = '\0';
-            strcpy(sh->RF->riga, buffer);
-            sh->RF->turn = 0;
+        if ((err = pthread_mutex_lock(&td->srf->lock)) != 0)
+            exit_with_err("pthread_mutex_lock", err);
 
-            if ((err = pthread_cond_signal(&sh->RF->cond[sh->RF->turn])) != 0)
-            {
+        while (td->srf->turn != 0)
+            if ((err = pthread_cond_wait(&td->srf->pcond[0], &td->srf->lock)) !=
+                0)
+                exit_with_err("pthread_cond_wait", err);
+
+        if (td->srf->done)
+        {
+            actual_reader++;
+
+            if (actual_reader > td->srf->nreader)
+                break;
+
+            td->srf->done = 0;
+            td->srf->turn = actual_reader;
+
+            if ((err = pthread_cond_signal(&td->srf->pcond[actual_reader])) !=
+                0)
                 exit_with_err("pthread_cond_signal", err);
-            }
-            if ((err = pthread_mutex_unlock(&sh->RF->lock)) != 0)
-            {
+
+            if ((err = pthread_mutex_unlock(&td->srf->lock)) != 0)
                 exit_with_err("pthread_mutex_unlock", err);
-            }
-        }
-        if ((err = pthread_mutex_lock(&sh->RF->lock)) != 0)
-        {
-            exit_with_err("pthread_mutex_lock", err);
-        }
-        while (sh->RF->turn != sh->thread_name)
-        {
-            if ((err = pthread_cond_wait(&sh->RF->cond[sh->thread_name], &sh->RF->lock)) != 0)
-            {
-                exit_with_err("pthread_cond_wait", err);
-            }
-        }
-        sh->RF->done = true;
-        if (sh->RF->fileattuale + 1 <= sh->numerofile)
-        {
-            sh->RF->fileattuale++;
+
+            continue;
         }
         else
-        {
-            break;
-        }
-        if ((err = pthread_cond_signal(&sh->RF->cond[0])) != 0)
-        {
+            strncpy(buffer, td->srf->buffer, BUFFER_SIZE);
+
+        td->srf->turn = actual_reader;
+
+        if ((err = pthread_cond_signal(&td->srf->pcond[actual_reader])) != 0)
             exit_with_err("pthread_cond_signal", err);
-        }
-        if ((err = pthread_mutex_unlock(&sh->RF->lock)) != 0)
-        {
+
+        if ((err = pthread_mutex_unlock(&td->srf->lock)) != 0)
             exit_with_err("pthread_mutex_unlock", err);
-        }
-    }
 
-    pthread_exit(NULL);
-}
-
-void Filtro(void *arg)
-{
-    int err;
-    thread_data *sh = (thread_data *)arg;
-
-    while (1)
-    {
-        if ((err = pthread_mutex_lock(&sh->RF->lock)) != 0)
+        if (filter_pass(td, buffer))
         {
-            exit_with_err("pthread_mutex_lock", err);
-        }
-        while (sh->thread_name != sh->RF->turn)
-        {
-            if ((err = pthread_cond_wait(&sh->RF->cond[sh->thread_name], &sh->RF->lock)) != 0)
-            {
-                exit_with_err("pthread_cond_wait", err);
-            }
-        }
-
-        if (applicaFiltro(sh->RF->parola, sh->RF->riga, sh->RF->i, sh->RF->v) == 1)
-        {
-            if ((err = pthread_mutex_lock(&sh->FM->lock2)) != 0)
-            {
+            // Inserimento nella struttura dati condivisa con il Writer
+            if ((err = pthread_mutex_lock(&td->sfw->lock)) != 0)
                 exit_with_err("pthread_mutex_lock", err);
-            }
 
-            while (sh->FM->done != 0)
-            {
-                if ((err = pthread_cond_wait(&sh->FM->cond_filter, &sh->FM->lock2)) != 0)
-                {
+            while (td->sfw->turn != 0)
+                if ((err = pthread_cond_wait(&td->sfw->pcond[0],
+                                             &td->sfw->lock)) != 0)
                     exit_with_err("pthread_cond_wait", err);
-                }
-            }
 
-            strcpy(sh->FM->riga, sh->RF->riga);
-            sh->FM->done = 1;
-            // printf("%s\n", sh->RF->riga);
+            strncpy(td->sfw->buffer, buffer, BUFFER_SIZE);
+            td->sfw->turn = 1;
 
-            if ((err = pthread_cond_signal(&sh->FM->cond_main)) != 0)
-            {
+            if ((err = pthread_cond_signal(&td->sfw->pcond[1])) != 0)
                 exit_with_err("pthread_cond_signal", err);
-            }
 
-            if ((err = pthread_mutex_unlock(&sh->FM->lock2)) != 0)
-            {
+            if ((err = pthread_mutex_unlock(&td->sfw->lock)) != 0)
                 exit_with_err("pthread_mutex_unlock", err);
-            }
-        }
-        if (sh->RF->fileattuale <= sh->numerofile)
-        {
-            sh->RF->turn = sh->RF->fileattuale;
-        }
-        else
-        {
-            break;
-        }
-        if ((err = pthread_cond_signal(&sh->RF->cond[sh->RF->turn])) != 0)
-        {
-            exit_with_err("pthread_cond_signal", err);
-        }
-        if ((err = pthread_mutex_unlock(&sh->RF->lock)) != 0)
-        {
-            exit_with_err("pthread_mutex_unlock", err);
         }
     }
+
+    if ((err = pthread_mutex_lock(&td->sfw->lock)) != 0)
+        exit_with_err("pthread_mutex_lock", err);
+
+    while (td->sfw->turn != 0)
+        if ((err = pthread_cond_wait(&td->sfw->pcond[0], &td->sfw->lock)) != 0)
+            exit_with_err("pthread_cond_wait", err);
+
+    td->sfw->done = 1;
+    td->sfw->turn = 1;
+
+    if ((err = pthread_cond_signal(&td->sfw->pcond[1])) != 0)
+        exit_with_err("pthread_cond_signal", err);
+
+    if ((err = pthread_mutex_unlock(&td->sfw->lock)) != 0)
+        exit_with_err("pthread_mutex_unlock", err);
+
+    if ((err = pthread_barrier_wait(&td->sfw->barrier)) > 0)
+        exit_with_err("pthread_barrier_wait", err);
+
     pthread_exit(NULL);
 }
 
-void Stampa(void *arg)
+void writer(void *arg)
 {
     int err;
-    thread_data *sh = (thread_data *)arg;
+    thread_data *td = (thread_data *)arg;
 
     while (1)
     {
-        if ((err = pthread_mutex_lock(&sh->FM->lock2)) != 0)
-        {
+        if ((err = pthread_mutex_lock(&td->sfw->lock)) != 0)
             exit_with_err("pthread_mutex_lock", err);
-        }
 
-        while (sh->FM->done == 0)
-        {
-            if ((err = pthread_cond_wait(&sh->FM->cond_main, &sh->FM->lock2)) != 0)
-            {
+        while (td->sfw->turn != 1)
+            if ((err = pthread_cond_wait(&td->sfw->pcond[1], &td->sfw->lock)) !=
+                0)
                 exit_with_err("pthread_cond_wait", err);
-            }
-        }
 
-        printf("%s\n", sh->FM->riga);
-        sh->FM->done = 0;
+        if (td->sfw->done)
+            break;
 
-        if ((err = pthread_cond_signal(&sh->FM->cond_filter)) != 0)
-        {
+        printf("%s\n", td->sfw->buffer);
+        td->sfw->turn = 0;
+
+        if ((err = pthread_cond_signal(&td->sfw->pcond[0])) != 0)
             exit_with_err("pthread_cond_signal", err);
-        }
 
-        if ((err = pthread_mutex_unlock(&sh->FM->lock2)) != 0)
-        {
+        if ((err = pthread_mutex_unlock(&td->sfw->lock)) != 0)
             exit_with_err("pthread_mutex_unlock", err);
-        }
     }
+
+    if ((err = pthread_barrier_wait(&td->sfw->barrier)) > 0)
+        exit_with_err("pthread_barrier_wait", err);
 
     pthread_exit(NULL);
-}
-
-void init_RF(sharedRF *sh, int numerofile)
-{
-    int err;
-    sh->fileattuale = 1;
-    sh->parola[0] = '\0';
-    sh->i = false;
-    sh->v = false;
-    sh->done = false;
-    sh->turn = 1;
-    sh->riga[0] = '\0';
-
-    if ((err = pthread_mutex_init(&sh->lock, NULL)) != 0)
-    {
-        exit_with_err("pthread_mutex_init", err);
-    }
-
-    for (int i = 0; i < numerofile + 1; i++)
-    {
-        if ((err = pthread_cond_init(&sh->cond[i], NULL)) != 0)
-        {
-            exit_with_err("pthread_cond_init", err);
-        }
-    }
-}
-
-void init_FM(sharedFM *sh)
-{
-    int err;
-    sh->done = false;
-    sh->riga[0] = '\0';
-    if ((err = pthread_mutex_init(&sh->lock2, NULL)) != 0)
-    {
-        exit_with_err("pthread_mutex_init", err);
-    }
-
-    if ((err = pthread_cond_init(&sh->cond_filter, NULL)) != 0)
-    {
-        exit_with_err("pthread_cond_init", err);
-    }
-
-    if ((err = pthread_cond_init(&sh->cond_main, NULL)) != 0)
-    {
-        exit_with_err("pthread_cond_init", err);
-    }
 }
 
 int main(int argc, char **argv)
 {
-    int err;
     if (argc < 3)
     {
-        printf("Usa %s -v -i <word> <nomefile>...\n", argv[0]);
+        printf("Usage: %s [-v] [-i] [word] <file-1> [file-2] [file-3] [...]\n",
+               argv[0]);
+
         exit(EXIT_FAILURE);
     }
 
-    bool v, i;
-    int cont = 1;
-    v = i = false;
+    int err;
+    int _from = 1;
+    char *word;
+    bool v_flag = 0;
+    bool i_flag = 0;
 
-    if ((strcmp(argv[1], "-v") == 0) || (strcmp(argv[2], "-v") == 0))
+    // parsing delle flag -i/-v (opzionali)
+    if (!strcmp(argv[1], "-v") || (argc > 2 && !strcmp(argv[2], "-v")))
     {
-        v = true;
-        cont++;
-    }
-    if ((strcmp(argv[1], "-i") == 0) || (strcmp(argv[2], "-i") == 0))
-    {
-        i = true;
-        cont++;
+        v_flag = 1;
+        _from++;
     }
 
-    char word[512];
-    strcpy(word, argv[cont]);
-
-    int numerofile;
-    numerofile = (argc - 1) - cont;
-
-    sharedRF *srf = malloc(sizeof(sharedRF));
-
-    srf->cond = malloc(sizeof(pthread_cond_t) * (numerofile + 1));
-    sharedFM *sfm = malloc(sizeof(sharedFM));
-
-    thread_data td[numerofile + 2];
-    init_RF(srf, numerofile);
-    init_FM(sfm);
-
-    // TH filtro
-    td[0].thread_name = 0;
-    td[0].numerofile = numerofile;
-    td[0].RF = srf;
-    td[0].FM = sfm;
-    td[0].RF->i = i;
-    td[0].RF->v = v;
-    strcpy(td[0].RF->parola, word);
-    if ((err = pthread_create(&td[0].tid, NULL, (void *)Filtro, (void *)&td[0])) != 0)
+    if (!strcmp(argv[1], "-i") || (argc > 2 && !strcmp(argv[2], "-i")))
     {
-        exit_with_err("pthread_create", err);
+        i_flag = 1;
+        _from++;
     }
 
-    // TH reader
-    for (int c = 1; c < numerofile + 1; c++)
-    {
-        td[c].thread_name = c;
-        td[c].numerofile = numerofile;
-        td[c].file = argv[cont + c];
-        td[c].RF = srf;
-        strcpy(td[c].RF->parola, word);
+    thread_data td[argc - _from + 1];
+    shared_rf *srf = malloc(sizeof(shared_rf));
+    shared_fw *sfw = malloc(sizeof(shared_fw));
+    init_shared_rf(srf, argc - _from - 1);
+    init_shared_fw(sfw);
 
-        if ((err = pthread_create(&td[c].tid, NULL, (void *)Lettore, (void *)&td[c])) != 0)
-        {
+    unsigned thread_data_index = 0;
+
+    // Reader
+    for (unsigned i = _from + 1; i < argc; i++)
+    {
+        td[thread_data_index].filename = argv[i];
+        td[thread_data_index].srf = srf;
+        td[thread_data_index].thread_n = thread_data_index + 1;
+
+        if ((err = pthread_create(&td[thread_data_index].tid, NULL,
+                                  (void *)reader, &td[thread_data_index])) != 0)
             exit_with_err("pthread_create", err);
-        }
+
+        thread_data_index++;
     }
 
-    // TH MAIN
-    td[numerofile + 1].thread_name = numerofile + 1;
-    td[numerofile + 1].numerofile = numerofile;
-    td[numerofile + 1].FM = sfm;
+    // Filterer
+    td[thread_data_index].i_flag = i_flag;
+    td[thread_data_index].v_flag = v_flag;
+    td[thread_data_index].sfw = sfw;
+    td[thread_data_index].srf = srf;
+    td[thread_data_index].thread_n = 0;
+    td[thread_data_index].word = argv[_from];
 
-    if ((err = pthread_create(&td[numerofile + 1].tid, NULL, (void *)Stampa, (void *)&td[numerofile + 1])) != 0)
-    {
+    if ((err = pthread_create(&td[thread_data_index].tid, NULL,
+                              (void *)filterer, &td[thread_data_index])) != 0)
         exit_with_err("pthread_create", err);
-    }
 
-    for (int i = 0; i < numerofile + 2; i++)
-    {
-        if ((err = pthread_join(td[i].tid, NULL)) != 0)
-        {
-            exit_with_err("pthread_join", err);
-        }
-    }
+    // Writer
+    thread_data_index++;
+    td[thread_data_index].sfw = sfw;
 
-    for (int i = 0; i < numerofile + 1; i++)
-    {
-        if ((err = pthread_cond_destroy(&srf->cond[i])) != 0)
-        {
-            exit_with_err("pthread_cond_destroy", err);
-        }
-    }
-    if ((err = pthread_mutex_destroy(&srf->lock)) != 0)
-    {
-        exit_with_err("pthread_mutex_destroy", err);
-    }
+    if ((err = pthread_create(&td[thread_data_index].tid, NULL, (void *)writer,
+                              &td[thread_data_index])) != 0)
+        exit_with_err("pthread_create", err);
 
-    if ((err = pthread_cond_destroy(&sfm->cond_filter)) != 0)
-    {
-        exit_with_err("pthread_cond_destroy", err);
-    }
+    for (unsigned i = 0; i < thread_data_index + 1; i++)
+        if ((err = pthread_detach(td[i].tid)) != 0)
+            exit_with_err("pthread_detach", err);
 
-    if ((err = pthread_cond_destroy(&sfm->cond_main)) != 0)
-    {
-        exit_with_err("pthread_cond_destroy", err);
-    }
+    if ((err = pthread_barrier_wait(&sfw->barrier)) > 0)
+        exit_with_err("pthread_barrier_wait", err);
 
-    if ((err = pthread_mutex_destroy(&sfm->lock2)) != 0)
-    {
-        exit_with_err("pthread_mutex_destroy", err);
-    }
-
-    free(srf);
-    free(sfm);
-
-    exit(EXIT_SUCCESS);
+    destroy_shared_rf(srf);
+    destroy_shared_fw(sfw);
 }
